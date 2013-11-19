000b7bedbfbbe45af1b672b79e9f54503984a360
hiren1612 <hiren1612@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-19 13:16:01 +0000

    update files
    git-svn-id: https://plugins.svn.wordpress.org/custom-users-order/tags/1.0@806816 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom_users_order.php
+++ custom_users_order.php
@@ -9,140 +9,112 @@ License: GPLv2 or later
 */
 
 register_activation_hook(__FILE__,'author_order');
-function author_order()
-{
-	 /* Function to add option name in wp_options table */
-	 $all_user_datas=get_users(); 
-	 $all_user_ids = "";
-	 foreach ($all_user_datas as $user) {
-	  $all_user_ids = $all_user_ids.','.$user->ID;
-		}
-	 $all_user_ids = substr($all_user_ids, 1);
-	 add_option("authordetails", $all_user_ids); 
-	
+function author_order(){
+	/* Function to add option name in wp_options table */
+	$all_user_datas=get_users(); 
+	$all_user_ids = "";
+	foreach ($all_user_datas as $user) {
+		$all_user_ids = $all_user_ids.','.$user->ID;
+	}
+	$all_user_ids = substr($all_user_ids, 1);
+	add_option("authordetails", $all_user_ids); 
+}
+add_action('user_register', 'update_authordetails');
+
+function update_authordetails($user_id){
+	$users = get_option('authordetails');
+	update_option("authordetails", $users.','.$user_id);
 }
 register_uninstall_hook(__FILE__,'author_uninstall');
-function author_uninstall()
-{
+
+function author_uninstall(){
 	delete_option('authordetails');
 }
-
 add_action('admin_menu', 'manageuser');
 
-function manageuser(){
-	
+function manageuser(){	
 	add_submenu_page( 'users.php', 'Custom Users Order Page', 'Custom Users Order', 'manage_options', 'customuserorder', 'customuserorder' ); 
-	
-	}
+}
 
-
-/**
- * Proper way to enqueue scripts and styles
- */
+/**  Proper way to enqueue scripts and styles  */
 function custom_order_scripts() {
 	wp_enqueue_style( 'customstyle', plugin_dir_url(__FILE__). 'css/style.css' );
 	wp_enqueue_script( 'jquery-ui-sortable' );
 	wp_enqueue_script( 'orderusers', plugin_dir_url(__FILE__). 'js/orderusers.js', array(), true );
 }
-
 add_action( 'admin_enqueue_scripts', 'custom_order_scripts' );
 
 function custom_display_style() {
 	wp_enqueue_style( 'customstyle', plugin_dir_url(__FILE__). 'css/customdisplay.css' );
 }
-
 add_action( 'wp_enqueue_scripts', 'custom_display_style' );
-
 add_filter('widget_text', 'do_shortcode');
-function author_listing($atts)
-{
 
- $options=get_option('authordetails');
- $user_id = explode(",", $options);
- if($atts[users]==""){$atts['users']=5;}  
- $wp_total_users_array= count_users();
- $wp_total_users=  $wp_total_users_array['total_users'];
- $plugin_content = '<div class="usersinfo">';
-  for($i=0;$i<$atts['users'];$i++)
-  {
-   $user_info = get_userdata($user_id[$i] );
-   $plugin_content = $plugin_content.'<div class="userlist">';
-   $plugin_content = $plugin_content. '<div class="useravtar">'.get_avatar($user_id[$i], 50 ).'</div>';
-   $plugin_content = $plugin_content. '<div class="usersnicename">';
-   $plugin_content = $plugin_content.$user_info->user_nicename;
-   $plugin_content = $plugin_content. '</div>';
-   $plugin_content = $plugin_content. '<div class="useremail">';
-   $plugin_content = $plugin_content.'<a href="mailto:'.$user_info->user_email.'">'.$user_info->user_email.'</a>';
-   $plugin_content = $plugin_content. '</div>';
-   $plugin_content = $plugin_content. "</div>";
-   if($i==($wp_total_users-1))
-    break;
-  }
- $plugin_content = $plugin_content. '</div>'; /*User info ends */
- return "{$plugin_content}";
+function author_listing($atts){
+	$options=get_option('authordetails');
+	$user_id = explode(",", $options);
+	if($atts[users]==""){$atts['users']=5;}  
+	$wp_total_users_array= count_users();
+	$wp_total_users=  $wp_total_users_array['total_users'];
+	$plugin_content = '<div class="usersinfo">';
+	for($i=0;$i<$atts['users'];$i++){
+		$user_info = get_userdata($user_id[$i] );
+		$plugin_content = $plugin_content.'<div class="userlist">';
+		$plugin_content = $plugin_content. '<div class="useravtar">'.get_avatar($user_id[$i], 50 ).'</div>';
+		$plugin_content = $plugin_content. '<div class="usersnicename">';
+		$plugin_content = $plugin_content.$user_info->user_nicename;
+		$plugin_content = $plugin_content. '</div>';
+		$plugin_content = $plugin_content. '<div class="useremail">';
+		$plugin_content = $plugin_content.'<a href="mailto:'.$user_info->user_email.'">'.$user_info->user_email.'</a>';
+		$plugin_content = $plugin_content. '</div>';
+		$plugin_content = $plugin_content. "</div>";
+		if($i==($wp_total_users-1))
+		break;
+	}
+	$plugin_content = $plugin_content. '</div>'; /*User info ends */
+	return "{$plugin_content}";
 }
 add_shortcode('author_listing','author_listing');
 
-function customuserorder()
-{
+function customuserorder(){
 	global $wpdb;
-	if($_REQUEST['usersid']!="")
-	{	
+	if($_REQUEST['usersid']!=""){	
 		$usersid = $_REQUEST['usersid'];
 		update_option( 'authordetails', $usersid ); 
-	}
-	
-?>
-    
+	}?>
     <div class='wrap'>
         <form name="frmCustomUser" method="post" action="?page=customuserorder">
-            <h2><?php _e('Custom Users Order', 'customuserorder') ?></h2>  
+			<h2><?php _e('Custom Users Order', 'customuserorder') ?></h2>  
             <div class="customusercontainer"> 
-           	<ul class="usersheading">
-            	<li class="lineitem"><span class=''>Username</span><span>Name</span><span>Email</span><span>Role</span></li>
-            </ul>
-            <ul id="UserOrderList">
-
-			<?php 
-			$options=get_option( 'authordetails');
-			$metadetails= explode(",", $options);
-			
-			for($i=0;$i<count($metadetails);$i++)
-			{
-				//echo get_the_author_meta( 'user_login', $metadetails[$i] );
-				if(get_the_author_meta( 'user_login', $metadetails[$i] ))
-				{
-					$userrole = new WP_User($metadetails[$i]);
-				//	echo $userrole->roles[0];
-					if($metadetails[0]!='')
-					{
-						echo "<li id='".$metadetails[$i]."' class='lineitem'><span>" .get_the_author_meta( 'display_name', $metadetails[$i] ). "</span><span>".get_the_author_meta( 'first_name', $metadetails[$i] )." ".get_the_author_meta( 'last_name', $metadetails[$i] )."</span><span>".get_the_author_meta( 'user_email', $metadetails[$i] )."</span><span class='userrole'>".$userrole->roles[0]."</span></li>";
-						
+				<ul class="usersheading">
+					<li class="lineitem"><span class=''>Username</span><span>Name</span><span>Email</span><span>Role</span></li>
+				</ul>
+				<ul id="UserOrderList">
+					<?php 
+					$options=get_option( 'authordetails');
+					$metadetails= explode(",", $options);
+					for($i=0;$i<count($metadetails);$i++){
+						if(get_the_author_meta( 'user_login', $metadetails[$i] )){
+							$userrole = new WP_User($metadetails[$i]);
+							if($metadetails[0]!=''){
+								echo "<li id='".$metadetails[$i]."' class='lineitem'><span>" .get_the_author_meta( 'display_name', $metadetails[$i] ). "</span><span>".get_the_author_meta( 'first_name', $metadetails[$i] )." ".get_the_author_meta( 'last_name', $metadetails[$i] )."</span><span>".get_the_author_meta( 'user_email', $metadetails[$i] )."</span><span class='userrole'>".$userrole->roles[0]."</span></li>";
+							}
+						}
+						else{					
+								$updatestring = get_option( 'authordetails');
+								$string = str_replace($metadetails[$i].',', "", $updatestring.',');
+								$string = substr($string,0, -1);
+								update_option( 'authordetails', $string); 
+							
+						}
 					}
-				}
-				else
-				{						
-						$updatestring = get_option( 'authordetails');
-						$string = str_replace($metadetails[$i].',', "", $updatestring.',');
-						$string = substr($string,0, -1);
-						update_option( 'authordetails', $string); 
-					
-				}
-				}
-				$results = get_users();
-				
-				foreach( $results as $user )
-					if (!(in_array($user->data->ID , $metadetails)))
-					{
+					$results = get_users();
+					foreach( $results as $user )
+					if (!(in_array($user->data->ID , $metadetails))){
 						$userrole = new WP_User($user->data->ID);
 						echo "<li id='{$user->data->ID}' class='lineitem'><span>" .get_the_author_meta( 'display_name', $user->data->ID ). "</span><span>".get_the_author_meta( 'first_name', $user->data->ID )." ".get_the_author_meta( 'last_name', $user->data->ID )."</span><span>".get_the_author_meta( 'user_email',$user->data->ID )."</span><span class='userrole'>".$userrole->roles[0]."</span></li>";
-					}	
-			
-			
-			
-            ?>
-            
-            </ul>
+					}?>
+				</ul>
             </div> <!-- customusercontainer -->
             <input type="hidden" name="usersid" />
             <input type="submit" name='send' value="Update" id='send' class="button-primary" />
@@ -153,5 +125,4 @@ function customuserorder()
     </div>
 <?php
 }
-
 ?>
